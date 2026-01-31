82ce47a540162e3009792e635e4ea23be069faec
VishalSonth <94623651+VishalSonth@users.noreply.github.com>
2026-01-31 14:08:08 +0100

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -37,21 +37,49 @@ class ModelTrainer:
                 test_array[:,:-1],
                 test_array[:,-1]
             )
-            models= {
+            models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
-                "LinearRegression" : LinearRegression(),
-                "k-Neighbors Classifier" : KNeighborsRegressor(),
-                "XGBClassifier" : XGBRegressor(),
-                "CatBoost Classifier" : CatBoostRegressor(verbose=False),
-                "AdaBoost Classifier" : AdaBoostRegressor(),
-
-
+                "Linear Regression": LinearRegression(),
+                "K-Neighbors Regressor": KNeighborsRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoost Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
             }
+            params = {
+    "Decision Tree": {
+        'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+    },
+    "Random Forest": {
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+    "Gradient Boosting": {
+        'learning_rate': [.1, .01, .05, .001],
+        'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+    "Linear Regression": {},
+    "K-Neighbors Regressor": {
+        'n_neighbors': [5, 7, 9, 11],
+    },
+    "XGBRegressor": {
+        'learning_rate': [.1, .01, .05, .001],
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+    "CatBoost Regressor": {
+        'depth': [6, 8, 10],
+        'learning_rate': [0.01, 0.05, 0.1],
+        'iterations': [30, 50, 100]
+    },
+    "AdaBoost Regressor": {
+        'learning_rate': [.1, .01, 0.5, .001],
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    }
+}
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+                                             models=models,param=params)
             
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
